a43913dedabb91c959ee83b8cb283a52953a6ec8
BGagnolo <benjamingagnolo@gmail.com>
2022-02-01 08:27:26 -0300

    2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          https://github.com/BenjaminGagnolo/reactappgagnolo.git HOLA 
+          https://github.com/BenjaminGagnolo/reactappgagnolo.git
         </a>
       </header>
     </div>
