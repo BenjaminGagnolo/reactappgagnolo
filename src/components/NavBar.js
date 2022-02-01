c1f4842f31f9c3674e1ad874d99c372ee86b7124
BGagnolo <benjamingagnolo@gmail.com>
2022-02-01 09:02:39 -0300

    add-NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css'
+
+
+function NavBar() {
+  return(<nav className="NavBar">
+      <div className="buttonNav">
+        <button>Remeras</button>
+        <button>Pantalones</button>
+        <button>Medias</button>
+        <button>Calzado</button>
+        <button>Buzos</button>
+        <button>Gorras</button>
+    </div>
+</nav> 
+  ) 
+}
+export default NavBar 
