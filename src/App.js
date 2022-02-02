5c4437c52d13fa002112b9f847e4f22df7da3278
BGagnolo <benjamingagnolo@gmail.com>
2022-02-02 11:02:31 -0300

    prueba-nav

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import { FaBeer } from 'react-icons/fa'; 
 
 function App() {
   return (
     
     <div className="App">
       <NavBar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/BenjaminGagnolo/reactappgagnolo.git"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          https://github.com/BenjaminGagnolo/reactappgagnolo.git
-        </a>
-      </header>
+     
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,28 @@
 import './NavBar.css'
-
-
+import {BsCart4} from "react-icons/bs"
+import {GiMuscleUp} from "react-icons/gi"
 function NavBar() {
-  return(<nav className="NavBar">
-      <div className="buttonNav">
-        <button>Remeras</button>
-        <button>Pantalones</button>
-        <button>Medias</button>
-        <button>Calzado</button>
-        <button>Buzos</button>
-        <button>Gorras</button>
+return(
+  <><div class="topnav" id="myTopnav">
+    <a href="#" className="active">Inicio</a>
+    <a href="#">Ofertas</a>
+    <a href="#">Contacto</a>
+    <div className="dropdown">
+      <button className="dropbtn">Prendas
+        <i className="fa fa-caret-down"></i>
+      </button>
+      <div className="dropdown-content">
+        <a href="#">Remeras</a>
+        <a href="#">Pantalones</a>
+        <a href="#">Calzado</a>
+      </div>
     </div>
-</nav> 
-  ) 
+    <a href="#">Sobre Nosotros</a>
+    <a href="#" id='carrito '><BsCart4 /></a>
+
+
+  </div><h1 className='Titulo'>SportShop  < GiMuscleUp /></h1></>
+
+      ) 
 }
-export default NavBar 
+export default NavBar;
